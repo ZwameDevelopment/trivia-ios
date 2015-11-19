c3f7bfb117b9da0f57b0932045a40aebbfd2fafe
Filipe Almeida <filipe.lmd@gmail.com>
2015-11-19 23:04:01 +0000

    initial commit

---
+++ trivia/trivia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trivia
+//
+//  Created by Filipe Almeida on 19/11/15.
+//  Copyright © 2015 Filipe Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
